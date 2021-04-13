472c455b265dced3bfe1d0c96759e7f1c32f887b
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-04-13 14:55:26 +0300

    remake controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,50 +8,57 @@ const generateJwt = (id, login, isAdmin) => {
   });
 };
 
-module.exports = {
-  registration: async (req, res) => {
-    const { login, password, isAdmin } = req.body;
+const registration = async (req, res) => {
+  const { login, password, isAdmin } = req.body;
 
-    try {
-      const candidate = await db.User.findOne({ where: { login } });
+  try {
+    const candidate = await db.User.findOne({ where: { login } });
 
-      if (candidate) {
-        return res.json({ message: 'Такой логин уже существует' });
-      }
-
-      const hashPassword = await bcrypt.hash(password, 5);
-
-      const user = await db.User.create({
-        login,
-        password: hashPassword,
-        isAdmin,
-      });
-
-      const token = generateJwt(user.id, user.login, user.isAdmin);
-
-      return res.json(token);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error);
+    if (candidate) {
+      return res.json({ message: 'Такой логин уже существует' });
     }
-  },
 
-  login: async (req, res) => {
-    const { login, password } = req.body;
-    const user = await db.User.findOne({ where: { login } });
-    if (!user) {
-      return res.json({ message: 'Пользователь не найден' });
-    }
-    let comparePassword = bcrypt.compareSync(password, user.password);
-    if (!comparePassword) {
-      return res.json({ message: 'Пароль неверен' });
-    }
+    const hashPassword = await bcrypt.hash(password, 5);
+
+    const user = await db.User.create({
+      login,
+      password: hashPassword,
+      isAdmin,
+    });
+
     const token = generateJwt(user.id, user.login, user.isAdmin);
 
     return res.json(token);
-  },
-  check: async (req, res) => {
-    const token = generateJwt(req.user.id, req.user.login, req.user.role);
-    return res.json(token);
-  },
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+const login = async (req, res) => {
+  const { login, password } = req.body;
+  const user = await db.User.findOne({ where: { login } });
+  if (!user) {
+    return res.json({ message: 'Пользователь не найден' });
+  }
+
+  let comparePassword = bcrypt.compareSync(password, user.password);
+
+  if (!comparePassword) {
+    return res.json({ message: 'Пароль неверен' });
+  }
+  const token = generateJwt(user.id, user.login, user.isAdmin);
+
+  return res.json(token);
+};
+
+const check = async (req, res) => {
+  const token = generateJwt(req.user.id, req.user.login, req.user.role);
+  return res.json(token);
+};
+
+module.exports = {
+  registration,
+  login,
+  check,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controllers = require('../controllers');
 const authMiddleware = require('../middleware/authMiddleware');
+const userController = require('../controllers/userController');
 
 router.post('/api/v1/create', controllers.create);
 router.get('/api/v1/health', controllers.health);
@@ -9,8 +10,8 @@ router.get('/api/v1/find/:id', controllers.findOne);
 router.delete('/api/v1/find/:id', controllers.deleteFilm);
 router.put('/api/v1/put/:id', controllers.put);
 
-router.post('/api/v1/sign-up', controllers.userController.registration);
-router.post('/api/v1/sign-in', controllers.userController.login);
-router.get('/api/v1/auth', authMiddleware, controllers.userController.check);
+router.post('/api/v1/sign-up', userController.registration);
+router.post('/api/v1/sign-in', userController.login);
+router.get('/api/v1/auth', authMiddleware, userController.check);
 
 module.exports = router;
